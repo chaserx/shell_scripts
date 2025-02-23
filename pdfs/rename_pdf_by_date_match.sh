9a61ea887771d986a4659556a81d352253d07a87
Chase Southard <chase.southard@gmail.com>
2025-02-23 15:57:58 -0500

    Improve simplicity by making the regex more complex

--- pdfs/rename_pdf_by_date_match.sh
+++ pdfs/rename_pdf_by_date_match.sh
@@ -1,4 +1,4 @@
-#! /opt/homebrew/bin/bash
+#! /usr/bin/env bash
 
 # Usage: ./rename_pdf_by_date_match.sh <pdf-file> <date>
 #
@@ -9,6 +9,18 @@
 #
 # the output file should be:
 # 2024-02-20_example.pdf
+
+SCRIPT_LOG=script_out.log
+touch $SCRIPT_LOG
+
+todayISO=$(gdate -I)
+todayUnix=$(date +%s)
+
+function DEBUG(){
+  local msg="$1"
+  local timeAndDate=`date`
+  echo "[$timeAndDate] [DEBUG]  $msg" >> $SCRIPT_LOG
+}
 
 
 # Check if pdfgrep is installed
@@ -35,39 +47,33 @@ if ! pdfinfo "$1" > /dev/null 2>&1; then
     exit 1
 fi
 
-# Extract the date from the pdf file into a variable globally across multiple lines
-
-# date format 02/20/2024
-slash_dates=$(pdfgrep -o -P -e "\d{2}/\d{2}/\d{4}" "$1")
-
-# date format February 20, 2024
-comma_dates=$(pdfgrep -o -P -e "\w+\s\d{2},\s\d{4}" "$1")
-
-# date format 2024-02-20
-iso_dates=$(pdfgrep -o -P -e "\d{4}-\d{2}-\d{2}" "$1")
-
-# date format 02-20-2024
-dash_dates=$(pdfgrep -o -P -e "\d{2}-\d{2}-\d{4}" "$1")
-
-if [ -z "$slash_dates" ] && [ -z "$comma_dates" ] && [ -z "$iso_dates" ] && [ -z "$dash_dates" ]; then
-    echo "No dates found in $1"
-    exit 1
-fi
-
-# Combine all the dates into a single variable
-dates="$slash_dates $comma_dates $iso_dates $dash_dates"
-
-# Reformat the dates to the ISO8601 format using Ruby
-dates=$(echo "$dates" | tail -n 1 | ruby -ne 'require "date"; puts Date.parse($_.strip).iso8601')
+# Get the directory of the original file
+dir=$(dirname "$1")
 
 # Get just the filename without path and extension
 filename=$(basename "$1" .pdf)
 
-# Get the directory of the original file
-dir=$(dirname "$1")
+# Extract the date from the pdf file into a variable globally across multiple lines
+# Return just the first result
+#
+# Match date formats: APR 13 2022 or APR 13, 2022 or April 13, 2022 or 4/13/2022 or 04/14/2022
+# https://regex101.com/r/9fPYPA/1
+# derived from https://stackoverflow.com/a/56081857
+#
+match_result=$(pdfgrep -i -m1 -o -P -e "(((1[0-2]|0?[1-9])\/(3[01]|[12][0-9]|0?[1-9])\/(?:[0-9]{2})?[0-9]{2})|((Jan(uary)?|Feb(ruary)?|Mar(ch)?|Apr(il)?|May|Jun(e)?|Jul(y)?|Aug(ust)?|Sep(tember)?|Oct(ober)?|Nov(ember)?|Dec(ember)?)\s+\d{1,2},?\s+\d{4}))" "$1")
+
+if [ -z "$match_result" ]; then
+    echo "No matching dates found in $1"
+
+    mv "$1" "${dir}/${todayISO}_x_${filename}_${todayUnix}.pdf"
+    DEBUG "Couldn't find a date. Renamed $1 to ${todayISO}_x_${filename}_${todayUnix}.pdf"
+    exit 0
+fi
+
+# Reformat the dates to the ISO8601 format using Ruby because we don't know the date format
+date=$(echo "$match_result" | tail -n 1 | ruby -ne 'require "date"; puts Date.parse($_.strip).iso8601')
 
 # Prepend the ISO8601 format to the filename and rename the file, keeping it in the same directory
-mv "$1" "${dir}/$(echo "$dates")_${filename}.pdf"
+mv "$1" "${dir}/${date}_${filename}_${todayUnix}.pdf"
 
-# write the rename to a log file
-echo "Renamed $1 to $(echo "$dates")_${filename}.pdf" >> rename_pdf_by_date_match.log
+DEBUG "Renamed $1 to ${date}_${filename}_${todayUnix}.pdf"
